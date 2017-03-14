2bc4b028b2137909600a879b968cbb4c1014ae72
Juan Carlos Romero Lorenzo <Juan Carlos Romero Lorenzo>
2017-03-13 19:01:54 -0600

    Creando vista Profesores del admin

--- resources/views/User/profesores.blade.php
+++ resources/views/User/profesores.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 <style>
     @import url('http://fonts.googleapis.com/css?family=Julius+Sans+One');
     @import url('https://fonts.googleapis.com/css?family=Anton');
     body{
         padding: 0;
         margin: 0;
-        background: #282E34;
     }
     .logo{
         position: absolute;
-        left: 50px;
+        left: 0px;
         width: 5%;
-        top: 5px;
+        top: -5px;
     }
     .log{
         color: #fff;
@@ -21,6 +21,8 @@
         font-family: 'Anton', sans-serif;
         letter-spacing: 3px;
         text-align: center;
+        top: 10px;
+        left: 70px;
     }
     .log1{
         color: #06bb84;
@@ -36,27 +38,34 @@
         border-color: #06bb84;  
     }
     .navv{
-    	height: 70px;
-        background: url(/Imagenes/bg.jpg);
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        overflow: hidden;
+    	height: 50px;
+        background: #263238;
     }
-    .prof{
-    	height: 200px;
+    .banner{
+    	height: 130px;
     	background-image: url(/Imagenes/prof.jpg);
-    	background-position: center;
     	background-repeat: no-repeat;
-    	background-size: cover;
-    	overflow: hidden;
+        background-size: 100%;
     }
     .nav1{
+        display: block;
+        color: #fff;
+        padding: 8px 16px;
+        text-decoration: none;
+    }
+    .nav2{
+        display: block;
+        color: #fff;
+        padding: 8px 16px;
+        text-decoration: none;
+        background-color: #546E7A;
+    }
+    .nav1:hover{
+        background-color: #455A64;
         color: #fff;
     }
     .select{
-        background-color: #06bb84;
-        border-radius: 3px;
+        background-color: #455A64;
     }
     .navu{
     	padding-top: 12px;
@@ -65,27 +74,61 @@
     	color: #fff;
     	font-family: 'Anton', sans-serif;
     	letter-spacing: 2px;
-    	padding-top: 30px;
     	font-size: 70px;
+        padding-left: 12%;
+    }
+    .menuvertical{
+        position: fixed;
+        padding-top: 0px;
+        background-color: #37474F;
+        width: 250px;
+        height: 100%;
+    }
+    .navvertical{
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        width: 250px;
+        font-style: none;
+    }
+    .subnav{
+        list-style-type: none;
+        margin: 0;
+        padding: 0px 0px;
+        font-style: none;
+    }
+    .menuvertical ul ul{
+        display: none;
+    }
+    .menuvertical a span{
+        margin-left: 10px;
     }
 </style>
 <div class="navv">
-	<div class="container">
-	    <img class="logo" src="Imagenes/Logo.png">
-	    <a href="{{ url('/') }}"><p class="log">SIGHO <span class="log1">&</span> MAPS</p></a>
-	</div>
-	<!--<div class="container">
-        <ul class="nav nav-pills navu">
-          <li><a class="nav1"href="{{ url('/homeu') }}">Inicio</a></li>
-          <li><a class="nav1"href="#">Horario</a></li>
-          <li class="select"><a class="nav1"href="#">Profesores</a></li>
-          <li><a class="nav1"href="#">Mapas</a></li>
-        </ul>
-    </div>-->
+    <img class="logo" src="Imagenes/Logo.png">
+    <a href="{{ url('/') }}"><p class="log">SIGHO <span class="log1">&</span> MAPS</p></a>
 </div>
-<div class="prof">
-	<div class="container">
-		<h1 class="profesor">Profesores</h1>
-	</div>
+<div class="banner col-md-12">
+    <div class="container">
+        <p class="profesor">Profesores</p>
+    </div>
 </div>
+<div class="menuvertical">
+    <ul class="navvertical">
+        <li><a class="nav1"href="#">Inicio</a></li>
+        <li class="select submenu"><a class="nav1"href="#">Profesores <span>+</span></a>
+            <ul class="subnav">
+                <li><a class="nav2" href="">Lista</a></li>
+                <li><a class="nav2" href="">Comentarios</a></li>
+            </ul>
+        </li>
+        <li><a class="nav1"href="#">Mapas</a></li>
+    </ul>
+</div>
+<script>
+$(".submenu").click(function(){
+    $(this).children("ul").slideToggle();
+})
+</script>
+
 @endsection
