5fc3e4837ed70b5c2e862b751515578cce606e99
Juan Carlos Romero Lorenzo <carlos_-619@hotmail.com>
2017-03-13 21:24:05 -0600

    Tabla en las vista profesores de User

--- resources/views/User/UserProfesores.blade.php
+++ resources/views/User/UserProfesores.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 <style>
     @import url('http://fonts.googleapis.com/css?family=Julius+Sans+One');
     @import url('https://fonts.googleapis.com/css?family=Anton');
     body{
         padding: 0;
         margin: 0;
-        font-family: 'Julius Sans One', sans-serif;
     }
     .color{
         border-color: #06bb84;  
@@ -43,6 +43,9 @@
         font-family: 'Anton', sans-serif;
         text-align: center;
     }
+    .letter{
+    	font-family: 'Julius Sans One', sans-serif;
+    }
 </style>
 <div class="parallax">
     <div class="container">
@@ -50,12 +53,33 @@
         <h1 class="portada">SIGHO <span class="yi">&</span> MAPS</h1>
     </div>
     <div class="container">
-        <ul class="nav nav-pills">
-          <li class="select "><a class="nav1"href="{{ url('User/Home') }}">Inicio</a></li>
+        <ul class="nav nav-pills letter">
+          <li class="nav1"><a class="nav1"href="{{ url('User/Home') }}">Inicio</a></li>
           <li><a class="nav1"href="#">Horario</a></li>
-          <li><a class="nav1"href="{{ url('User/Profesores') }}">Profesores</a></li>
+          <li><a class="select nav1"href="{{ url('User/Profesores') }}">Profesores</a></li>
           <li><a class="nav1"href="#">Mapas</a></li>
         </ul>
     </div>
     <hr class="color"/>
 </div>
+<br/>
+<br/>
+<div class="container tabla">
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Profesor</th>
+				<th>Informacion</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row">1</th>
+				<th>Carlos</th>
+				<th><button type="button" class="btn btn-success">Ver más</button></th>
+			</tr>
+		</tbody>
+	</table>
+</div>
+@endsection
